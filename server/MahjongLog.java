a46aeee1720a0c716d490bdcd5f2056614ba6826
cnyaw <wayncc@gmail.com>
2023-04-26 13:30:49 +0800

    Created new log file when date is changed.

--- server/MahjongLog.java
+++ server/MahjongLog.java
@@ -15,27 +15,19 @@ public class MahjongLog {
 
   static final String LOG_PATH = "./log/";
 
+  String currDate;                      // Saved current date for latest log file name.
   FileWriter log;                       // Log file.
 
   MahjongLog() throws Exception {
 
     //
-    // Create log file.
+    // Create log file folder.
     //
-
-    Calendar cal = Calendar.getInstance();
 
     File dirLog = new File(LOG_PATH);
     if (!dirLog.exists()) {
       dirLog.mkdir();
     }
-
-    log = new FileWriter(
-                LOG_PATH + cal.get(Calendar.YEAR) +
-                "-" + (1 + cal.get(Calendar.MONTH)) +
-                "-" + cal.get(Calendar.DAY_OF_MONTH) +
-                "-MjSvr.log",
-                true);
   }
 
   //
@@ -43,11 +35,29 @@ public class MahjongLog {
   //
 
   void PrintLog(String mess) throws IOException {
+    checkLogFileDate();
     String head = (new Date()).toString() + ", ";
     System.out.print(head + mess);
     log.write(head + mess);
     log.flush();
   }
+
+  void checkLogFileDate() throws IOException {
+    String today = getDateString();
+    if (today != currDate) {
+      log = new FileWriter(LOG_PATH + today + "-MjSvr.log", true);
+      currDate = today;
+    }
+  }
+
+  String getDateString() {
+    Calendar cal = Calendar.getInstance();
+    String s = cal.get(Calendar.YEAR) + "-" +
+               (1 + cal.get(Calendar.MONTH)) + "-" +
+               cal.get(Calendar.DAY_OF_MONTH);
+    return s;
+  }
+
 } // MahjongLog
 
 // end of MahjongLog.java
